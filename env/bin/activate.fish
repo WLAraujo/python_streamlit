cecc85c2cdf6aa85f3bff9dc9707fb0c7202a382
WLAraujo <lima.wesleyaraujo@gmail.com>
2021-09-08 16:23:07 -0300

    Usando venv

--- env/bin/activate.fish
+++ env/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/wesleyla/Streamlit/env"
+set -gx VIRTUAL_ENV "/home/wesleyaraujo/pessoal/Streamlit/env"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
