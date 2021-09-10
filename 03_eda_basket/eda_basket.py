213abee9ad3588a21925f5c499fab04899e40fb3
WLAraujo <lima.wesleyaraujo@gmail.com>
2021-09-10 10:34:30 -0300

    apps

--- 03_eda_basket/eda_basket.py
+++ 03_eda_basket/eda_basket.py
@@ -29,3 +29,51 @@ def carregar_dados(ano):
     html = pd.read_html(url, header = 0)
     df = html[0]
     raw = df.drop(df[df.Age == 'Age'].index)
+    raw = raw.fillna(0)
+    estatisticas = raw.drop(['Rk'], axis = 1)
+    return estatisticas
+estatisticas_jogadores = carregar_dados(ano)
+
+# Seleção de times através da barra de parâmetros
+times_ordenados = sorted(estatisticas_jogadores['Tm'].unique())
+times_selecionados = st.sidebar.multiselect('Times', times_ordenados, times_ordenados)
+
+# Seleção de posições através de barra de parâmetros
+posicoes = sorted(estatisticas_jogadores['Pos'].unique())
+pos_sel = st.sidebar.multiselect('Posições', posicoes, posicoes)
+
+# Filtragem dos dados
+df_times_pos = estatisticas_jogadores[(estatisticas_jogadores['Tm'].isin(times_selecionados)) & (estatisticas_jogadores['Pos'].isin(pos_sel))]
+
+# Mudando os tipos de dados numéricos
+lista_colunas = ['Age', 'G', 'GS', 'MP', 'FG', 'FGA', 'FG%', '3P', '3PA', '3P%', '2P', '2P%', 'eFG%', 'FT', 'FTA', 'FT%', 'ORB', 'DRB', 'TRB', 'TRB', 'AST', 'STL', 'BLK', 'TOV', 'PF', 'PTS']
+for coluna in lista_colunas:
+    df_times_pos[coluna] = pd.to_numeric(df_times_pos[coluna])
+
+# Display das informações
+linhas = df_times_pos.shape
+st.header('Informações de Jogadores dos Times e Posições Selecionadas')
+st.write(f"""
+A tabela tem {df_times_pos.shape[0]} **linhas** e {df_times_pos.shape[1]} **colunas**.
+""")
+st.dataframe(df_times_pos)
+
+# Download dos dados
+def download_dados(df):
+    csv = df.to_csv(index = False)
+    cod_b64 = base64.b64encode(csv.encode()).decode()
+    href = f'<a href = "data:file/csv;base64,{cod_b64}" download = "estatisticas.csv"> Download CSV </a>'
+    return href
+
+st.markdown(download_dados(df_times_pos), unsafe_allow_html = True)
+    
+# Mostrando heatmap de correlação
+if st.button("Heatmap de Intercorrelação"):
+    st.header("Heatmap de Intercorrelação")
+    df = pd.DataFrame
+    df = df_times_pos.copy(deep = True)
+    corr = df.corr()
+    fig, ax = plt.subplots(figsize = (7,5))
+    ax = sns.heatmap(corr, vmax = 1, vmin= -1)
+    st.pyplot(fig)
+    
